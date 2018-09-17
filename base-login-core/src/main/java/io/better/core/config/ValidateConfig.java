730bbc83a8e8ce4e77d6b3420c8cc67616124cde
better <chenmingyang>
2018-09-17 17:03:12 +0800

    修改依赖,添加browser和app的security配置类

--- base-login-core/src/main/java/io/better/core/config/ValidateConfig.java
+++ base-login-core/src/main/java/io/better/core/config/ValidateConfig.java
@@ -1,11 +1,5 @@
 package io.better.core.config;
 
-import io.better.core.properties.SecurityProperties;
-import io.better.core.validate.generator.impl.DefaultImageCodeGenerator;
-import io.better.core.validate.generator.impl.DefaultSmsCodeGenerator;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -15,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
  * @date create in 2018/9/13 下午3:59
  */
 @Configuration
-@EnableConfigurationProperties(SecurityProperties.class)
 public class ValidateConfig {
 
 
