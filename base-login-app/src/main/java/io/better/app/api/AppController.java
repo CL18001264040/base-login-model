44577de105f8aff2664c4c1ccc24ddcec3b288c2
better <chenmingyang>
2018-09-17 17:06:40 +0800

    添加browser和app对应的controller

---
+++ base-login-app/src/main/java/io/better/app/api/AppController.java
@@ -0,0 +1,15 @@
+package io.better.app.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The type App controller.
+ *
+ * @author better
+ * @date create in 2018/9/17 下午5:04
+ */
+@RestController
+@RequestMapping(value = "/api/app")
+public class AppController {
+}
